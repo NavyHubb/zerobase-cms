b69198ba2e661c310233e6095c25e0fc7c42383c
NavyHubb <sngjae972@gmail.com>
2023-03-21 18:26:05 +0900

    EmailSendService 테스트 코드 작성

--- user/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
+++ user/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
@@ -1,8 +1,33 @@
 package com.zerobase.cms.user.client.service;
 
+import com.zerobase.cms.user.client.MailgunClient;
+import com.zerobase.cms.user.mailgun.SendMailForm;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EmailSendServiceTest {
-    
+
+    @Autowired
+    private MailgunClient mailgunClient;
+
+    @Test
+    void EmailTest() {
+        //given
+        ResponseEntity<String> responseEntity =
+                mailgunClient.sendEmail(SendMailForm.builder()
+                                .from("abc@gmail.com")
+                                .to("seongjae972@gmail.com")
+                                .subject("This is Me")
+                                .text("It isn, in fact.")
+                                .build());
+
+        //then
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
 }
